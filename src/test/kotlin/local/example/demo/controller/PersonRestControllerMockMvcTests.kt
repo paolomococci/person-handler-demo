c6cd87262c4c221a317c310b71d41ab02b45c4a2
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-10-08 07:31:30 +0200

    Update PersonRestControllerMockMvcTests.kt

--- src/test/kotlin/local/example/demo/controller/PersonRestControllerMockMvcTests.kt
+++ src/test/kotlin/local/example/demo/controller/PersonRestControllerMockMvcTests.kt
@@ -18,20 +18,155 @@
 
 package local.example.demo.controller
 
+import local.example.demo.repository.PersonRepository
+import org.hamcrest.Matchers.containsString
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class PersonRestControllerMockMvcTests {
 
+    @Autowired
+    private val mockMvc: MockMvc? = null
+
+    @Autowired
+    private val personRepository: PersonRepository? = null
+
+    private val paul: String = "{\"name\":\"Paul\",\"surname\":\"Smith\",\"age\":\"35\"}"
+
     @Test
     @Throws(Exception::class)
-    fun `sample verify `() {
+    fun `verify existence`() {
+        mockMvc!!.perform(get("/api/persons"))
+                .andDo(print())
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$._embedded.persons").exists())
+    }
 
+    @Test
+    @Throws(Exception::class)
+    fun `create test`() {
+        mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+                .andExpect(header().string("Location", containsString("api/persons/")))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `retrieve test`() {
+        val mvcResult = mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+                .andReturn()
+        val result = mvcResult.response.getHeader("Location")
+        mockMvc.perform(get(result!!))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.name").value("Paul"))
+                .andExpect(jsonPath("$.surname").value("Smith"))
+                .andExpect(jsonPath("$.age").value(35))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `update test`() {
+        val mvcResult = mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+                .andReturn()
+        val result = mvcResult.response.getHeader("Location")
+        mockMvc.perform(put(result!!)
+                .content("{\"name\":\"Jessy\",\"surname\":\"Smith\",\"age\":\"25\"}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        mockMvc.perform(get(result))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.name").value("Jessy"))
+                .andExpect(jsonPath("$.surname").value("Smith"))
+                .andExpect(jsonPath("$.age").value(25))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `partial update test`() {
+        val mvcResult = mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+                .andReturn()
+        val result = mvcResult.response.getHeader("Location")
+        mockMvc.perform(patch(result!!)
+                .content("{\"age\":\"36\"}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        mockMvc.perform(get(result))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.age").value(36))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `delete test`() {
+        val mvcResult = mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+                .andReturn()
+        val result = mvcResult.response.getHeader("Location")
+        mockMvc.perform(delete(result!!)).andExpect(status().isNoContent)
+        mockMvc.perform(get(result)).andExpect(status().isNotFound)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `verify find by path id`() {
+        mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        val id = personRepository!!.findByName("Paul")[0].id
+        mockMvc.perform(get("/api/persons/{id}", id))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.name").value("Paul"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `verify find by name`() {
+        mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        mockMvc.perform(get("/api/persons/name/{name}", "Paul"))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$._embedded.persons[0].age").value(35))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `verify find by surname`() {
+        mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        mockMvc.perform(get("/api/persons/surname/{surname}", "Smith"))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$._embedded.persons[0].name").value("Paul"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `verify find by age`() {
+        mockMvc!!.perform(post("/api/persons")
+                .content(paul).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated)
+        mockMvc.perform(get("/api/persons/age/{age}", 35))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$._embedded.persons[0].name").value("Paul"))
     }
 }
