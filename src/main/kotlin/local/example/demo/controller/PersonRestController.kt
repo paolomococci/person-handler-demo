1db7094b57b2bdf02f1b3a196a28a18af9f61f19
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-10-08 07:39:59 +0200

    Update PersonRestController.kt

--- src/main/kotlin/local/example/demo/controller/PersonRestController.kt
+++ src/main/kotlin/local/example/demo/controller/PersonRestController.kt
@@ -19,9 +19,13 @@
 package local.example.demo.controller
 
 import local.example.demo.assembler.PersonResourceAssembler
+import local.example.demo.exception.PersonNotFoundException
 import local.example.demo.model.Person
 import local.example.demo.repository.PersonRepository
 import org.springframework.hateoas.Resource
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.net.URI
@@ -43,13 +47,96 @@ class PersonRestController(
 
     @GetMapping("/{id}")
     @Throws(URISyntaxException::class)
-    internal fun read(@PathVariable id: Long?) {
-        
+    internal fun read(@PathVariable id: Long?): Resource<Person> {
+        val person = personRepository.findById(id!!)
+                .orElseThrow { PersonNotFoundException(id) }
+        return personResourceAssembler.toResource(person)
     }
 
     @GetMapping
     @Throws(URISyntaxException::class)
-    internal fun readAll() {
-        
+    internal fun readAll(): Resources<Resource<Person>> {
+        val persons = personRepository.findAll()
+                .asSequence()
+                .map(personResourceAssembler::toResource).toList()
+        return Resources(persons,
+                linkTo(methodOn(PersonRestController::class.java).readAll()).withSelfRel())
+    }
+
+    @PutMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun update(@RequestBody update: Person, @PathVariable id: Long?): ResponseEntity<*> {
+        val updated = personRepository.findById(id!!)
+                .map { temp ->
+                    temp.name = update.name
+                    temp.surname = update.surname
+                    if (update.age!! in 0..140) {
+                        temp.age = update.age
+                    }
+                    personRepository.save(temp)
+                }
+                .orElseGet {
+                    personRepository.save(update)
+                }
+        val resource = personResourceAssembler.toResource(updated)
+        return ResponseEntity.created(URI(resource.id.expand().href)).body(resource)
+    }
+
+    @PatchMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun partialUpdate(@RequestBody update: Person, @PathVariable id: Long?): ResponseEntity<*> {
+        val updated = personRepository.findById(id!!)
+                .map { temp ->
+                    if (!update.name.isNullOrBlank()) temp.name = update.name
+                    if (!update.surname.isNullOrBlank()) temp.surname = update.surname
+                    if (update.age != null) {
+                        if (update.age!! in 0..140) {
+                            temp.age = update.age
+                        }
+                    }
+                    personRepository.save(temp)
+                }
+                .orElseGet {
+                    personRepository.save(update)
+                }
+        val resource = personResourceAssembler.toResource(updated)
+        return ResponseEntity.created(URI(resource.id.expand().href)).body(resource)
+    }
+
+    @DeleteMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun cancel(@PathVariable id: Long?): ResponseEntity<*> {
+        if (id != null) personRepository.deleteById(id)
+        return ResponseEntity.noContent().build<Any>()
+    }
+
+    @GetMapping("/age/{age}")
+    @Throws(URISyntaxException::class)
+    internal fun searchByAge(@PathVariable age: Int?): Resources<Resource<Person>> {
+        val persons = personRepository.findByAge(age!!)
+                .asSequence()
+                .map(personResourceAssembler::toResource).toList()
+        return Resources(persons,
+                linkTo(methodOn(PersonRestController::class.java).searchByAge(age)).withSelfRel())
+    }
+
+    @GetMapping("/name/{name}")
+    @Throws(URISyntaxException::class)
+    internal fun searchByName(@PathVariable name: String?): Resources<Resource<Person>> {
+        val persons = personRepository.findByName(name!!)
+                .asSequence()
+                .map(personResourceAssembler::toResource).toList()
+        return Resources(persons,
+                linkTo(methodOn(PersonRestController::class.java).searchByName(name)).withSelfRel())
+    }
+
+    @GetMapping("/surname/{surname}")
+    @Throws(URISyntaxException::class)
+    internal fun searchBySurname(@PathVariable surname: String?): Resources<Resource<Person>> {
+        val persons = personRepository.findBySurname(surname!!)
+                .asSequence()
+                .map(personResourceAssembler::toResource).toList()
+        return Resources(persons,
+                linkTo(methodOn(PersonRestController::class.java).searchBySurname(surname)).withSelfRel())
     }
 }
